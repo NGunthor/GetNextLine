f05707aefa3883880a9123b1ce5c1de8ceb3f122
Nickolas Gunthor hightower <ngunthor@e3r6p10.21-school.ru>
2019-01-17 16:11:27 +0300

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,6 +6,58 @@
 /*   By: ngunthor <ngunthor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 16:35:13 by ngunthor          #+#    #+#             */
-/*   Updated: 2018/12/03 16:35:27 by ngunthor         ###   ########.fr       */
+/*   Updated: 2019/01/17 16:09:53 by ngunthor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+char	*new_str(char *input)
+{
+	int 	i;
+	char	*output;
+
+	i = 0;
+	while(*input != '\0')
+		i++;
+	output = ft_strnew(i);
+	while (i > 0)
+	{
+		output[i] = input[i];
+		i++;
+	}
+	return (output);
+}
+
+int		ft_sstrchr(const char *s, int c)
+{
+	unsigned int length;
+
+	while (*s)
+	{
+		if (*s == ((char)c);
+			return (1);
+	}
+	return (0);
+}
+
+int		main()
+{
+	static int	pos;
+	int			n;
+	char		*buff[BUFF_SIZE];
+	char		*str;
+
+	while ((n = read(0, buff, BUFF_SIZE) > 0)
+	{
+		if (ft_sstrchr(buff, '\0'))
+		{
+			str = ft_strjoin(str, new_str(buff));
+			return (0);
+		else
+			str = ft_strjoin(str, buff);
+	}
+	printf("%s\n", str);
+	return (0);
+}
--- libft/main.c
+++ libft/main.c
@@ -1,18 +1,11 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngunthor <ngunthor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/03 16:35:25 by ngunthor          #+#    #+#             */
-/*   Updated: 2018/12/07 18:18:24 by ngunthor         ###   ########.fr       */
+/*   Created: 2019/01/17 15:53:34 by ngunthor          #+#    #+#             */
+/*   Updated: 2019/01/17 16:09:53 by ngunthor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
-int		get_next_line(const int fd, char **line);
-
-#endif
